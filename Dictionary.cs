1095a6fd66caab1ee14d805c30a4dcfb2049fb89
Anna Myslytska <amyslytska@kse.org.ua>
2023-03-22 10:20:35 +0200

    Initial commit: reuploaded the repo due to problems

---
+++ Dictionary.cs
@@ -0,0 +1,60 @@
+﻿namespace HashTable;
+
+public class Dictionary
+{
+    private const int InitialSize = 1000000;
+
+    private LinkedList[] _buckets = new LinkedList[InitialSize];
+        
+    public void Add(string key, string value)
+    {
+        int hash = CalculateHash(key);
+        var index = hash % _buckets.Length;
+
+        if (_buckets[index] == null)
+        {
+            _buckets[index] = new LinkedList();
+        }
+        _buckets[index].Add(new KeyValuePair(key, value));
+    }
+
+    public void Remove(string key)
+    {
+        int hash = CalculateHash(key);
+        int index = hash % _buckets.Length;
+
+        if (_buckets[index] != null)
+        {
+            _buckets[index].RemoveByKey(key);
+        }        
+    }
+
+    public string Get(string key)
+    {
+        int hash = CalculateHash(key);
+        int index = hash % _buckets.Length;
+
+        if (_buckets[index] != null)
+        {
+            KeyValuePair pair = _buckets[index].GetItemWithKey(key);
+            if (pair != null)
+            {
+                return pair.Value;
+            }
+        }
+        return null; 
+    }
+
+
+    private int CalculateHash(string key)
+    {
+        int hash = 0;
+        foreach (char c in key)
+        {
+            hash = hash * 13 + c;
+        }
+
+        hash = hash + key.Length;
+        return hash;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace HashTable;
+using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var dictionary = new Dictionary();
+        var lines = File.ReadAllLines("C:/Users/annmy/RiderProjects/Hash Table/testDict.txt");
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(';');
+            string key = parts[0].Trim();
+            string value = parts[1].Trim();
+            dictionary.Add(key, value);
+        }
+
+        Console.WriteLine("Enter a word:");
+        string word = Console.ReadLine();
+        string meaning = dictionary.Get(word);
+        if (meaning != null)
+        {
+            Console.WriteLine($"'the {word}' means: {meaning}");
+        }
+        else
+        {
+            Console.WriteLine($"'{word}' was not found.");
+        }
+    }
+}
