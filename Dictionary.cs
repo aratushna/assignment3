637b9947a5fe30bb4f68d468997d77f5aaa9c34c
aratushna <94103037+aratushna@users.noreply.github.com>
2023-03-23 14:54:07 +0200

    Update Dictionary.cs

--- Dictionary.cs
+++ Dictionary.cs
@@ -1,22 +1,56 @@
-﻿namespace HashTable;
+namespace assignment3;
 
+/*Маючи зв'язний список ми можемо реалізувати вже і основну структуру - словник.
+Пам'ятайте, що додавання у словник відбувається за наступним алгоритмом:
+Отримати ключ та значення для додавання
+1)Знайти хеш від ключу
+2)Знайти відповідну корзину (bucket) для додавання - 
+  для цього достатньо просто знайти залишок від ділення хешу на кількість бакетів
+3)Якщо в цій корзині вже є зв'язний список - додати новий елемент в кінець,
+ якщо ще ні - створити список та додати
+4)Доступ до елемента за ключем:
+Отримати ключ
+1)Знайти хеш від ключу
+2)Знайти відповідну корзину (bucket) для додавання
+3)Якщо в цій корзині вже є зв'язний список - спробувати дістати зі списку елемент з таким ключем,
+  якщо ще ні - повернути null*/
 public class Dictionary
 {
-    private const int InitialSize = 1000000;
-
-    private LinkedList[] _buckets = new LinkedList[InitialSize];
+    private const int InitialSize = 10;
+    private List <LinkedList> buckets_10 = new List <LinkedList>(InitialSize);
+    private List<LinkedList> _buckets = new List <LinkedList>();
+    private int num_buckets = 0;
+    private double load_factor = 0.7;
+    private int num_load_facktor = 0;
         
     public void Add(string key, string value)
     {
-        int hash = CalculateHash(key);
-        var index = hash % _buckets.Length;
 
-        if (_buckets[index] == null)
+        if (num_buckets == 0)_buckets = buckets_10;
+        if ((num_buckets / _buckets.Count) >= load_factor) 
         {
-            _buckets[index] = new LinkedList();
+            _buckets.AddRange(buckets_10); 
+            num_load_facktor ++;
+            num_buckets = num_load_facktor * 10 - 1;  
         }
-        _buckets[index].Add(new KeyValuePair(key, value));
+
+        int hash = CalculateHash(key);
+        var index = (hash % _buckets.Length) + (10 * num_load_facktor);  
+  
+
+        var value = _buckets[index];
+
+
+        if (value == null)
+        {
+            value = new LinkedList();
+        }
+        value.Add(new KeyValuePair(key, value));
+
+        num_buckets ++;
     }
+        
+
 
     public void Remove(string key)
     {
@@ -26,35 +60,6 @@ public class Dictionary
         if (_buckets[index] != null)
         {
             _buckets[index].RemoveByKey(key);
-        }        
+        } 
+        num_buckets --;       
     }
-
-    public string Get(string key)
-    {
-        int hash = CalculateHash(key);
-        int index = hash % _buckets.Length;
-
-        if (_buckets[index] != null)
-        {
-            KeyValuePair pair = _buckets[index].GetItemWithKey(key);
-            if (pair != null)
-            {
-                return pair.Value;
-            }
-        }
-        return null; 
-    }
-
-
-    private int CalculateHash(string key)
-    {
-        int hash = 0;
-        foreach (char c in key)
-        {
-            hash = hash * 13 + c;
-        }
-
-        hash = hash + key.Length;
-        return hash;
-    }
-}
